b95e0c4e5acdac0e2ff0da5b072eb42e31b605a9
Christopher C. Santos <closedman@hotmail.com>
2023-06-15 21:49:08 -0300

    fix

--- src/modulo2.js
+++ src/modulo2.js
@@ -2,23 +2,33 @@ const listaDeBruxos = require("../src/lista.js");
 
 console.table(listaDeBruxos);
 
-function percorreLista(lista) {
+function percorrerLista(lista) {
   let lifeFase;
+  let n = 0;
+  let x = 0;
+  let y = 0;
 
   lista.forEach((bruxo) => {
     if (typeof bruxo.idade === "number" && bruxo.idade >= 0 && bruxo.idade < 12) {
         lifeFase = "Pequeno(a) Bruxinho(a)";
+        n++
     } else if (typeof bruxo.idade === "number" && bruxo.idade >= 12 && bruxo.idade < 18) {
       lifeFase = "Jovem Bruxo(a)";
+        x++
     } else if (typeof bruxo.idade === "number" && bruxo.idade > 18) {
       lifeFase = "Bruxo(a)";
+        y++
     } else {
-      console.log("Idade não é válida!");
+      console.log("Idade inválida!");
     }
     console.log(`${lifeFase} ${bruxo.nome}, você está sendo convocado(a) para escola de magia e bruxaria de Hogwarts`);
+
   });
   console.log(`\nO número total de bruxos(as) listados(as) é: ${lista.length}`);
+  console.log(n)
+  console.log(x)
+  console.log(y)
   return lista.length;
 }
 
-module.exports = percorreLista;
+module.exports = percorrerLista;
